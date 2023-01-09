a9cc4ac105c7ecf1b5ea2e6e6263066a262d983c
Dravya Bansal <ac.dravya44409@gmail.com>
2023-01-09 09:25:16 +0530

    done

--- hiking/src/components/index.js
+++
@@ -1,4 +0,0 @@
-export { default as Blog } from './Blog';
-export { default as Companies } from './Companies';
-export { default as Header } from './Header';
-export { default as Review } from './Review';
